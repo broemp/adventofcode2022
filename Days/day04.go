c531c372245c00ad70f6429bab761ad40d58200e
broemp <derbrommi@live.de>
2022-12-06 00:11:32 +0100

    Modified: Import Day04 Data

--- Days/day04.go
+++ Days/day04.go
@@ -1,7 +1,14 @@
 package Days
 
+import (
+	Helper "github.com/broemp/adventofcode2022/Helper"
+)
+
 func Day04Main() {
-	overlapFinder()
+	dataPath := "./Data/day04.txt"
+	data := Helper.ParseFile(dataPath)
+
+	overlapFinder(data)
 }
 
 func overlapFinder(data []string) {
